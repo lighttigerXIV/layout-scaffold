192c5b6c50743a374fa682396b33ff3e8c368fbd
lighttigerxiv <lighttigerxiv@gmail.com>
2024-11-21 19:20:46 +0000

    Add commentary

--- layout-scaffold/src/main/java/com/lighttigerxiv/layout_scaffold/Devices.kt
+++ layout-scaffold/src/main/java/com/lighttigerxiv/layout_scaffold/Devices.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.compose.runtime.Composable
 
+/** Returns true if the available size is of a phone */
 @Composable
 fun isPhone(): Boolean {
     return (inPortrait() && isCompactWidth() && isMediumHeight())
@@ -12,11 +13,13 @@ fun isPhone(): Boolean {
             || (inLandscape() && isCompactHeight())
 }
 
+/** Returns true if the available size is of a tablet or unfolded big foldable like Pixel 9 Pro Fold */
 @Composable
 fun isTablet(): Boolean {
     return !isPhone()
 }
 
+/** Returns true if the device has folding capabilities */
 fun isFoldable(context: Context): Boolean {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) context.packageManager.hasSystemFeature(
         PackageManager.FEATURE_SENSOR_HINGE_ANGLE
